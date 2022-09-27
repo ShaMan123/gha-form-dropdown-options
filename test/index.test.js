ed35bc95e167688a318655cc8a812f1cf9934829
ShaMan123 <shacharnen@gmail.com>
2022-09-27 18:29:50 +0300

    Update index.test.js

--- test/index.test.js
+++ test/index.test.js
@@ -8,20 +8,6 @@ import { fileURLToPath } from 'node:url';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-function getBranch() {
-	if (!process.env.CI) {
-		return execSync('git branch --show-current').toString().trim();
-	}
-	switch (process.env.GITHUB_EVENT_NAME) {
-		case 'pull_request':
-			return process.env.GITHUB_HEAD_REF;
-		case 'release':
-			return 'main';
-		default:
-			return process.env.GITHUB_REF;
-	}
-}
-
 function parseInputs(inputs) {
 	return Object.keys(inputs).reduce((parsed, key) => {
 		parsed[`INPUT_${key.toUpperCase()}`] =
@@ -33,56 +19,41 @@ function parseInputs(inputs) {
 }
 
 describe('action', function () {
+	const expected = path.resolve(__dirname, 'expected.yml');
+	const template = path.resolve(__dirname, 'template.yml');
+	const test = path.resolve(__dirname, 'temp.yml');
 	this.timeout(5000);
 	this.beforeAll(() => {
 		// https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token
 		dotenv.config();
 	});
-	it('workflow_dispatch', async function () {
-		await assert.doesNotReject(
-			github
-				.getOctokit(process.env.GITHUB_TOKEN)
-				.rest.actions.createWorkflowDispatch({
-					owner: 'ShaMan123',
-					repo: 'gha-form-dropdown-options',
-					ref: getBranch(),
-					workflow_id: '.github/workflows/update_bug_report.yml',
-				}),
+	this.beforeEach(() => {
+		writeFileSync(test, readFileSync(template));
+		assert.equal(
+			readFileSync(test).toString(),
+			readFileSync(template).toString(),
+			'should prepare test',
 		);
 	});
+	this.afterEach(() => {
+		unlinkSync(test);
+		assert.ok(!existsSync(test), 'should cleanup test');
+	});
 
-	describe('manual trigger', function () {
-		const expected = path.resolve(__dirname, 'expected.yml');
-		const template = path.resolve(__dirname, 'template.yml');
-		const test = path.resolve(__dirname, 'temp.yml');
-		this.beforeEach(() => {
-			writeFileSync(test, readFileSync(template));
-			assert.equal(
-				readFileSync(test).toString(),
-				readFileSync(template).toString(),
-				'should prepare test',
-			);
-		});
-		this.afterEach(() => {
-			unlinkSync(test);
-			assert.ok(!existsSync(test), 'should cleanup test');
-		});
-
-		it('passing tags', async function () {
-			const inputs = {
-				// github_token,
-				form: test,
-				dropdown: 'version',
-				options: ['1.2.3', '4.5.6', '7.8.9'],
-				dry_run: true,
-			};
-			Object.assign(process.env, parseInputs(inputs));
-			await import('../dist/main.cjs');
-			assert.strictEqual(
-				readFileSync(test).toString().replace(/\s/gm, ''),
-				readFileSync(expected).toString().replace(/\s/gm, ''),
-				'output should match',
-			);
-		});
+	it('passing options', async function () {
+		const inputs = {
+			// github_token,
+			form: test,
+			dropdown: 'version',
+			options: ['1.2.3', '4.5.6', '7.8.9'],
+			dry_run: true,
+		};
+		Object.assign(process.env, parseInputs(inputs));
+		await import('../dist/main.cjs');
+		assert.strictEqual(
+			readFileSync(test).toString().replace(/\s/gm, ''),
+			readFileSync(expected).toString().replace(/\s/gm, ''),
+			'output should match',
+		);
 	});
 });
