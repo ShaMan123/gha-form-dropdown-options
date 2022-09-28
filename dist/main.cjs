b25457d7ad9e108bed05fc6b45c3227d19c6c771
ShaMan123 <shacharnen@gmail.com>
2022-09-28 09:38:20 +0300

    Update main.cjs

--- dist/main.cjs
+++ dist/main.cjs
@@ -6489,8 +6489,40 @@ var jsYaml = {
 	safeDump: safeDump
 };
 
+function readYAML(file, template) {
+	console.log({ file, template });
+	if (template && fs__default["default"].existsSync(file)) {
+		// avoid overriding existing options by prefill template with actual form
+		const templateContent = jsYaml.load(fs__default["default"].readFileSync(template).toString());
+		const content = jsYaml.load(fs__default["default"].readFileSync(file).toString());
+		templateContent.body.forEach((entry, index) => {
+			if (entry.type !== 'dropdown') return;
+			const {
+				attributes: { options },
+			} = entry;
+			if (
+				!options ||
+				!options.length ||
+				(Array.isArray(options) &&
+					options.every((option) => {
+						try {
+							return !option || !JSON.parse(option);
+						} catch (error) {
+							return true;
+						}
+					}))
+			) {
+				templateContent.body[index].attributes.options =
+					content.body[index].attributes.options;
+			}
+		});
+		return templateContent;
+	}
+	return jsYaml.load(fs__default["default"].readFileSync(template || file).toString());
+}
+
 function writeYAML(file, template, dropdownId, tags) {
-	const content = jsYaml.load(fs__default["default"].readFileSync(template || file).toString());
+	const content = readYAML(file, template);
 	const found = content.body.find(
 		(entry) => entry.id === dropdownId && entry.type === 'dropdown',
 	);
